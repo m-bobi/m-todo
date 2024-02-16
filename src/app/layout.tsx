f610f50bf82c822bfa57cf5858d6e9c056df77f2
m-bobi <melosbobi11@gmail.com>
2024-02-16 22:06:49 +0100

    + Todo App with NextJS, Prisma and Tailwind

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "M-Todo"
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-white`}>{children}</body>
+    </html>
+  );
+}
