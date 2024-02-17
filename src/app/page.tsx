a02e22adb0ba00430c5303c3ca59016e4cfcbb17
m-bobi <melosbobi11@gmail.com>
2024-02-17 20:01:33 +0100

    + Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,43 @@
 import Link from "next/link";
 import Image from "next/image";
+import { prisma } from "@/db";
+import { TodoItem } from "@/components/TodoItem";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+async function getTodos() {
+  return await prisma.todo.findMany();
+}
+
+async function toggleTodo(id: string, complete: boolean) {
+  "use server";
+
+  await prisma.todo.update({ where: { id }, data: { complete } });
+  redirect("..");
+}
+
+async function deleteTodo(id: string) {
+  "use server";
+  await prisma.todo.delete({ where: { id } });
+  redirect("..");
+}
+
+async function CreateTodo(data: FormData) {
+  "use server";
+
+  const title = data.get("title")?.valueOf();
+
+  if (typeof title !== "string" || title.length === 0) {
+    throw new Error("Invalid Title!");
+  }
+
+  await prisma.todo.create({ data: { title, complete: false } });
+  redirect("..");
+}
+
+
+export default async function Home() {
+  const todos = getTodos();
+
   return (
     <>
       <header className="flex justify-between items-center">
@@ -28,6 +64,38 @@ export default function Home() {
         </Link>
       </header>
       <hr className="w-auto" />
+
+      <form
+        action={CreateTodo}
+        className="flex gap-2 flex-col translate-y-52 sm:translate-y-52"
+      >
+        <input
+          type="text"
+          name="title"
+          placeholder="Create TODO"
+          className="border border-t-0 border-l-0 border-r-0 w-60 border-slate-300 bg-transparent rounded px-2 py-1 outline-none"
+        />
+
+        <div className="gap-2 flex sm:hidden">
+          <button
+            type="submit"
+            className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"
+          >
+            Create{" "}
+          </button>
+        </div>
+      </form>
+
+      <ul className="pl-4 flex flex-col translate-y-96 md:translate-y-96 lg:translate-y-96 sm:translate-y-96 flex-wrap text-wrap">
+        {(await todos).map((todo) => (
+          <TodoItem
+            key={todo.id}
+            {...todo}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        ))}
+      </ul>
     </>
   );
 }
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { useState } from "react";
+import { FaRegTrashCan } from "react-icons/fa6";
+
+type TodoItemProps = {
+  id: string;
+  title: string;
+  complete: boolean;
+  toggleTodo: (id: string, complete: boolean) => void;
+  deleteTodo: (id: string) => void;
+};
+
+
+export function TodoItem({
+  id,
+  title,
+  complete,
+  toggleTodo,
+  deleteTodo,
+}: TodoItemProps) {
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const handleDelete = async () => {
+    setIsDeleting(true);
+    try {
+      await deleteTodo(id);
+    } catch (error) {
+      console.error("Error deleting todo:", error);
+    }
+    setIsDeleting(false);
+  };
+
+  return (
+    <li className="flex items-center gap-2">
+      <input
+        id={id}
+        type="checkbox"
+        className="peer cursor-pointer appearance-none w-6 h-6 border-2 border-black rounded checked:bg-black checked:border-transparent focus:outline-none"
+        defaultChecked={complete}
+        onChange={(e) => toggleTodo(id, e.target.checked)}
+      />
+      <label
+        htmlFor={id}
+        className={`peer-checked:line-through peer-checked:text-slate-500 text-xl cursor-pointer break-words ${
+          isDeleting ? "opacity-50" : ""
+        }`}
+        style={{ maxWidth: "15rem" }}
+      >
+        {title}
+      </label>
+      <button
+        onClick={handleDelete}
+        disabled={isDeleting}
+        className={`text-red-600 cursor-pointer ml-36 focus:outline-none ${
+          isDeleting ? "opacity-50" : ""
+        }`}
+      >
+        {isDeleting ? "Deleting..." : <FaRegTrashCan />}
+      </button>
+    </li>
+  );
+}
